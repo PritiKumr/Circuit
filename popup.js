915525f716689f2baa4a19191a21c1e392abf43f
PritiKumr <priti922474@gmail.com>
2016-08-01 12:08:28 +0530

    Changes

--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
 window.onload = function(){
     document.getElementById("show_time_div").style.visibility = "hidden";
     document.getElementById("time_input_box").addEventListener("click", time_input_box);
-    if(!window.jQuery)
-		{
-		   alert("No");
-		}
-		$("#pickMe").datepicker();
+    chrome.tabs.getSelected(null, function(tab) {
+		  if tab.url
+
+	});
 }
 
 function time_input_box(){
 	document.getElementById("show_time_div").style.visibility = "visible";
 }
+
+
 
