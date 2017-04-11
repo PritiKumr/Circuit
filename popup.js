b657dca565e7b5c02f9f664e4e3c0df2fc3ae1c5
Steve Robinson <steve.rob@me.com>
2017-04-11 15:52:14 +0530

    preventing undefined being displayed when there are no timecodes

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@ chrome.runtime.onConnect.addListener(function(port) {
   port.postMessage({command: "request_timecodes"});
   port.onMessage.addListener(function(msg) {
     if (msg.command == 'displayTimecodes') {
-      document.getElementById("start_time").value = msg.startTime;
-      document.getElementById("end_time").value   = msg.endTime;  
+      document.getElementById("start_time").value = msg.startTime || '';
+      document.getElementById("end_time").value   = msg.endTime || '';  
     };
   });
 });
